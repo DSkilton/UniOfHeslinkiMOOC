834e823cf5822fdfdd980e9624e8e8f9d4c014e1
unknown <the_skiltons@hotmail.com>
2021-09-03 20:39:21 +0100

    completed and passed Part04_02.YourFirstBankTransfer

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,11 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        
+        Account Duncan = new Account("Duncan", 100);
+        
+        Duncan.deposit(20.00);
+        System.out.println(Duncan);
+       
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,13 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        Account a1 = new Account("Matthews account", 1000);
+        Account a2 = new Account("My account", 0);
+        
+        a1.withdrawal(100);
+        a2.deposit(100);
+        
+        System.out.println(a1);
+        System.out.println(a2);
     }
 }
