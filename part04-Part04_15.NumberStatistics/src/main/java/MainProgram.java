5bd624964fc1cb64d2a1112f7d184114c7cf3ab7
unknown <the_skiltons@hotmail.com>
2022-01-16 20:24:07 +0000

    completed part 4 challenge 15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,6 +5,33 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Statistics sum = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+        
+        System.out.println("Enter numbers");
+        
+        while(true){
+            int input = scanner.nextInt();
+            
+            if(input == -1){
+                System.out.println("Sum: " + sum.sum());
+                System.out.println("Sum of even numbers: " + even.sum());
+                System.out.println("Sum of odd numbers: " + odd.sum());
+                break;
+            } else {
+                sum.addNumber(input);
+                
+                if(input % 2 == 0){
+                    even.addNumber(input);
+                } else {
+                    odd.addNumber(input);
+                }// end of odd even
+                
+            }// end of if 
+            
+        }// end of while
+        
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,28 @@
-
 public class Statistics {
-}
+    private int count;
+    private int sum;
+    
+    public Statistics(){
+        
+    }
+    
+    public void addNumber(int number){
+        this.count++;
+        this.sum += number;
+    }
+    
+    public int getCount(){
+        return this.count;
+    }
+    
+    public int sum(){
+        return sum;
+    }
+    
+    public double average(){
+        if(this.sum != 0 && this.count !=0 ){
+            return 1.0 * this.sum / this.count;
+        }
+        return 0.0; 
+    }
+}// end of the class 
