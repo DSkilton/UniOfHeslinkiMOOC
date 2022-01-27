2d609c69187c58510bc8d9fd4d64630bef17bf97
unknown <the_skiltons@hotmail.com>
2022-01-27 20:14:28 +0000

    completed part 4 challenge 17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -9,6 +9,20 @@ public class Items {
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+                
+        while(true){
+            System.out.println("Enter an item, empty will stop");
+            String item = scanner.nextLine();
+            
+            if(item.isEmpty()){
+                break;
+            }
+            items.add(new Item(item));
+        }// end of while
 
-    }
+        for(Item item : items){
+            System.out.println(item);
+        }
+        
+    }// end main method
 }
