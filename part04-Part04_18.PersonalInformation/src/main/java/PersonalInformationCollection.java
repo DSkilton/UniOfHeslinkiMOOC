91d72661c1419747df640d79105461e08d218923
unknown <the_skiltons@hotmail.com>
2022-01-27 20:41:52 +0000

    completed part 4 exercise 18

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,26 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+   
+            String firstName = scanner.nextLine();
+            
+            if(firstName.isEmpty()){
+                break;
+            }// end of if
+            
 
+            String lastName = scanner.nextLine();
+            
+  
+            String idNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNumber));
+        }
+        
+        for(PersonalInformation pi : infoCollection){
+            System.out.println(pi.getFirstName() + " " + pi.getLastName());
+        }
     }
 }
