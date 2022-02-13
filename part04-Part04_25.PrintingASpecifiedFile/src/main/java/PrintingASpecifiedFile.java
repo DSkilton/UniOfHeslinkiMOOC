e50324504d3b5b00b899dc86bc74a38264ba3a20
unknown <the_skiltons@hotmail.com>
2022-02-13 15:10:48 +0000

    completed part 4 exercise 25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -6,6 +7,20 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String fileName;
+        
+        fileName = scanner.nextLine();
+        findFile(fileName);
+    }
 
+    public static void findFile(String fileName) {
+        try ( Scanner SC = new Scanner(Paths.get(fileName))) {
+            while (SC.hasNextLine()) {
+                String row = SC.nextLine();
+                System.out.println(row);
+            }
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex);
+        }
     }
 }
