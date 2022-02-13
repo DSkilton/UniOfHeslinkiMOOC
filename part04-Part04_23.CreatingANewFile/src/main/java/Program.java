48fd4545e10d7b942669ab9790254f7ffa1a4693
unknown <the_skiltons@hotmail.com>
2022-02-13 15:05:07 +0000

    completed exercise 4 part 24

--- part04-Part04_23.CreatingANewFile/src/main/java/Program.java
+++ part04-Part04_23.CreatingANewFile/src/main/java/Program.java
@@ -1,6 +1,8 @@
+
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
-        // you don't need to do anything here...
     }
 }
--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,27 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrintingAFile {
 
     public static void main(String[] args) {
+        printFile();
 
     }
+    
+    public static void printFile(){
+        try (Scanner SC = new Scanner(Paths.get("data.txt"))){
+            while(SC.hasNextLine()){
+                String row = SC.nextLine();
+                System.out.println(row);
+            }
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex);
+        }
+    
+    }
+    
 }
