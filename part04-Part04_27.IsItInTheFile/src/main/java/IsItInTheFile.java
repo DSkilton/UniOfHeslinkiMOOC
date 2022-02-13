76e3a138c6538960f4d5c8eebda9a5f9bdd3892a
unknown <the_skiltons@hotmail.com>
2022-02-13 15:53:02 +0000

    completed exercise 4 part 27

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -5,13 +5,35 @@ import java.util.Scanner;
 public class IsItInTheFile {
 
     public static void main(String[] args) {
+        boolean isFound = false;
+        String row = "";
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        try(Scanner fileScan = new Scanner(Paths.get(file))){
+            while(fileScan.hasNextLine()){
+                row = fileScan.nextLine();
+                
+                if(searchedFor.equals(row)){
+                    isFound = true;
+                }//end of if
+            }
+                if(isFound){
+                    System.out.println("Found!");
+                } else {
+                    System.out.println(row);
+                    System.out.println("Not found.");
+                }
+   
+        } catch (Exception e){
+            System.out.println("Reading the file " + file + " failed.");
+        
+        }
 
     }
 }
