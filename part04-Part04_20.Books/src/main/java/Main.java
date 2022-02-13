e98a3eadae5a24164d96d6c15d6fb81cb7a48f69
unknown <the_skiltons@hotmail.com>
2022-02-13 13:43:08 +0000

    Completed part for exercise 21

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -35,6 +35,5 @@ public class Main {
                 System.out.println(book.getInstanceTitle());
             }
         }
-
     }
 }
--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,20 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = "";
+        int counter = 0;
+        
+        while(true){
+            
+            if(input.equals("end")){
+                break;
+            }
+            
+            input = scanner.nextLine();
+            counter++;
+        }
+        
+        System.out.println(--counter);
 
     }
 }
