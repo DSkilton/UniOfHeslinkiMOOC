803507bbc16bade2c562f13fc0d07e8eb4df0086
unknown <the_skiltons@hotmail.com>
2022-03-06 11:05:21 +0000

    working v1

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -1,11 +1,9 @@
-
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SportStatistics {
-    private static int count = 0, wins = 0, losses = 0;
-    
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -15,11 +13,16 @@ public class SportStatistics {
         ArrayList<Team> records = readRecordsFromFile(file);
 
         System.out.println("Team: ");
-        String teamName = scan.nextLine();        
+        String teamName = scan.nextLine();
+
+        int counter = 0;
+        int wins = 0;
+        int losses = 0;
 
         for (Team team : records) {
+
             if (team.getHomeTeam().contains(teamName)) {
-                count += 1;
+                counter += 1;
 
                 if (team.getHomeScore() >= team.getAwayScore()) {
                     wins += 1;
@@ -31,7 +34,7 @@ public class SportStatistics {
 
                 team.playGame();
             } else if (team.getAwayTeam().contains(teamName)) {
-                count += 1;
+                counter += 1;
 
                 if (team.getAwayScore() >= team.getHomeScore()) {
                     wins += 1;
@@ -43,9 +46,10 @@ public class SportStatistics {
 
                 team.playGame();
             }
+
         }
 
-        System.out.println("Games: " + count);
+        System.out.println("Games: " + counter);
         System.out.println("Wins: " + wins);
         System.out.println("Losses: " + losses);
     }
@@ -53,7 +57,7 @@ public class SportStatistics {
     public static ArrayList<Team> readRecordsFromFile(String file) {
         ArrayList<Team> teams = new ArrayList<>();
 
-        try ( Scanner scanner = new Scanner(Paths.get(file))) {
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -66,10 +70,13 @@ public class SportStatistics {
                 int awayScore = Integer.valueOf(parts[3]);
 
                 teams.add(new Team(homeTeam, awayTeam, homeScore, awayScore));
+
             }
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+
         return teams;
     }
 
