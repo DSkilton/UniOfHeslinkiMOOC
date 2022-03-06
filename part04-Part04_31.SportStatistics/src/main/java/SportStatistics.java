8e6ffc46e73da6051b3fb1b5e8c373f4fb9ee188
unknown <the_skiltons@hotmail.com>
2022-03-06 11:10:10 +0000

    part 4 exercise 31 completed

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SportStatistics {
+    
 
     public static void main(String[] args) {
+        int count = 0, wins = 0, losses = 0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("File: ");
@@ -15,14 +17,10 @@ public class SportStatistics {
         System.out.println("Team: ");
         String teamName = scan.nextLine();
 
-        int counter = 0;
-        int wins = 0;
-        int losses = 0;
-
         for (Team team : records) {
 
             if (team.getHomeTeam().contains(teamName)) {
-                counter += 1;
+                count += 1;
 
                 if (team.getHomeScore() >= team.getAwayScore()) {
                     wins += 1;
@@ -34,7 +32,7 @@ public class SportStatistics {
 
                 team.playGame();
             } else if (team.getAwayTeam().contains(teamName)) {
-                counter += 1;
+                count += 1;
 
                 if (team.getAwayScore() >= team.getHomeScore()) {
                     wins += 1;
@@ -49,7 +47,7 @@ public class SportStatistics {
 
         }
 
-        System.out.println("Games: " + counter);
+        System.out.println("Games: " + count);
         System.out.println("Wins: " + wins);
         System.out.println("Losses: " + losses);
     }
@@ -58,7 +56,6 @@ public class SportStatistics {
         ArrayList<Team> teams = new ArrayList<>();
 
         try (Scanner scanner = new Scanner(Paths.get(file))) {
-
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
 
