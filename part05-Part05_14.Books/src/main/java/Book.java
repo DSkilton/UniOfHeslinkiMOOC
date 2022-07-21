05a0d986d3fdd1ace5063201aa82dae862294085
unknown <the_skiltons@hotmail.com>
2022-07-21 21:39:41 +0100

    completed part 5 exercise 14

--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -1,8 +1,8 @@
 
 public class Book {
-
     private String name;
     private int publicationYear;
+    Book comparedBook;
 
     public Book(String name, int publicationYear) {
         this.name = name;
@@ -17,4 +17,21 @@ public class Book {
         return publicationYear;
     }
 
+    public boolean equals(Object compared){
+        if(this == compared){
+            return true;
+        }
+        
+        if(!(compared instanceof Book)){
+            return false;
+        }
+        
+        comparedBook = (Book)compared;
+        
+        if(this.name.equals(comparedBook.getName()) && this.publicationYear == comparedBook.getPublicationYear()){
+            return true;
+        }
+        
+        return false;
+    }
 }
