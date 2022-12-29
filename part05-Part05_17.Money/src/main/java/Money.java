b7ac38e328cb7d591f2ee62f00d2147783b20684
unknown <the_skiltons@hotmail.com>
2022-12-29 20:45:49 +0000

    completed part 5 exercise 17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -15,21 +15,61 @@ public class Money {
         this.cents = cents;
     }
 
+    public Money plus(Money plus) {
+        int totalCents = this.cents + plus.cents;
+        int totalEuros = this.euros + plus.euros;
+        Money newMoneyObj = new Money(totalEuros, totalCents);
+//      class   object       constructor 
+
+        return newMoneyObj;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (euros < compared.euros) {
+            return true;
+        }
+
+        if (euros == compared.euros) {
+            if (cents < compared.cents) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Money minus(Money decreaser){
+        int totalEuros = this.euros - decreaser.euros; 
+        int totalCents = this.cents - decreaser.cents; 
+        System.out.println(totalEuros);
+        if(totalEuros >= 0){
+            if (totalCents < 0){
+                totalEuros = totalEuros - 1;
+                totalCents = totalCents + 100;
+            }
+        }  else {
+            totalEuros = 0;
+            totalCents = 0;
+        }
+        
+        Money newMoneyObj = new Money(totalEuros , totalCents);
+        return newMoneyObj;
+    }
+
     public int euros() {
-        return this.euros;
+        return euros;
     }
 
     public int cents() {
-        return this.cents;
+        return cents;
     }
 
     public String toString() {
         String zero = "";
-        if (this.cents < 10) {
+        if (cents < 10) {
             zero = "0";
         }
 
-        return this.euros + "." + zero + this.cents + "e";
+        return euros + "." + zero + cents + "e";
     }
 
 }
